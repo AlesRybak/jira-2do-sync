258df5e47c59ff86586ce51cfed9699a3274514c
Aleš Rybák <ales.rybak@lundegaard.eu>
2020-04-28 16:49:36 +0200

    fix: fixed JQL

--- src/main/kotlin/cz/alry/twodo/jirasync/cli/Constants.kt
+++ src/main/kotlin/cz/alry/twodo/jirasync/cli/Constants.kt
@@ -4,6 +4,6 @@ package cz.alry.twodo.jirasync.cli
  * @author Ales Rybak(ales.rybak@lundegaard.eu)
  */
 
-const val MY_OPEN_ISSUES_JQL: String = "status in (Unstarted, \"In Progress\", Reopened, \"To Do\", \"To Be Reviewed\", Backlog, Open) AND assignee in (currentUser()) order by updated DESC"
+const val MY_OPEN_ISSUES_JQL: String = "status in (\"In Progress\", Reopened, \"To Do\", \"To Be Reviewed\", Backlog, Open) AND assignee in (currentUser()) order by updated DESC"
 
 const val FILE_SYNCED_ISSUES: String = "synced-issues.txt"
